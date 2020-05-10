dc235e3d5f03a61d8ac72483ce756cfdda819664
bhavenjain <61782538+bhavenjain@users.noreply.github.com>
2020-05-10 12:34:34 +0530

    Update menu.h

--- main/menu.h
+++ main/menu.h
@@ -1,76 +1,63 @@
 
+//login info check
+bool check_forLogin(char username[50], char password[20]){
 
-//DISPLAY MENU FUNCTION
-int once =1,data_days=7;
-char country_name[50];
+  char username1[] = "admin";
+  char password1[] = "vaccine";
 
-void display_welcome_menu()
-{
-    clrscr();
-    int j=16,i=j+1;char option;
-    menu_start :
-    gotoxy(62,10);
-    printf("!!!Welcome to the Covid-19 Tracker!!!\n");
-    gotoxy(30,j-1);
-    printf("How do you wish to proceed?");
-    gotoxy(30,j+1);
-    printf("    Server Log-in");
-    gotoxy(30,j+2);
-    printf("    User Log-in");
-    gotoxy(30,i);
-    printf("-->");
-    option = _getch();
-    switch(option)
-    {
-        case 80 :   if(i<j+2)
-                        i++;
-                    clrscr();
-                    goto menu_start;
-                    break;
-        case 72 :   if(i>j+1)
-                        i--;
-                    clrscr();
-                    goto menu_start;
-                    break;
-        case 13 :   gotoxy(30,j+6);
-                    break;
-        default :   goto menu_start;
+  int check_username = 0;
+
+    printf("%s",username1);
+    if( strcmp(username1,username) == 0){
+        check_username = 1;
+        if(strcmp(password1,password) == 0)
+            return 1;
+        else{
+            printf("Password does not match...\n");
+            return 0;
+        }
     }
-    switch(i-j)
-    {
-        case 1  :   printf("CALL ADD FUNCTION");
-                    display_server_menu();
-                    break;
-        case 2  :   printf("CALL USER FUNCTION");
-                    display_user_menu();
-                    break;
-        default :   goto menu_start;
+  if(check_username == 0){
+        printf("Username does not match...\n");
+        return 0;
     }
-
 }
 
+
+
+//DISPLAY MENU FUNCTION
+int data_days=7;
+char country_name[50];
+COORD coord = {0,0};
+
+void gotoxy(int x, int y)
+{
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
+}
 
 void display_server_menu()
 {
     clrscr();
-
-    char username[20],password[20];
+    char username[50];
+    char password[20];
     int p=0;
     gotoxy(30,12);
     printf("USERNAME          :");
     gotoxy(30,15);
     printf("PASSWORD          :");
     gotoxy(60,12);
-    fgets(username, 20, stdin);
+    fgets(username,50,stdin);
+    strtok(username, "\n");
+    fflush(stdin);
     gotoxy(60,15);
     do
     {
         password[p] = _getch();
         if(password[p] == '\b')
         {
-
-            if(p!=0)
-                {
+            if(p!=0){
                     p -= 2;
                     printf("\b \b");
                 }
@@ -82,26 +69,40 @@ void display_server_menu()
             printf("*");
         }
         p++;
+
     }while(password[p-1]!='\r');
     password[p-1]='\0';
-
+    fflush(stdin);
     gotoxy(30,20);
-    //printf("%s\n\n%s",username,password);
-    _getch();
-    display_country_menu(1);
+    //printf("%ld %s",username,password);
 
+    int verify = check_forLogin(username,password);
+
+    _getch();
+
+    if(verify == 1)
+        display_country_menu(1);
+    else{
+        display_server_menu();
+        printf("Try Again");
+    }
 }
+
+
+
 void display_user_menu()
 {
     clrscr();
     display_country_menu(0);
 }
+
+
 void display_country_menu(int check)
 {
     clrscr();
     int j=15,i=j+1;char option;
     menu_start :
-    gotoxy(75,10);
+    gotoxy(33,10);
     printf("!!!Welcome!!!\n");
     gotoxy(30,j-1);
     printf("Choose any country:-");
@@ -152,12 +153,58 @@ void display_country_menu(int check)
         default:    goto menu_start;
     }
 
-    if(check == 1 )
-    {
-        function_add();
+    if(check == 1 ){
+      function_add();
     }
-    else
-    {
-        FileHandle();
-    }
+    else;
+        //userInput();
 }
+
+
+
+
+void display_welcome_menu()
+{
+    clrscr();
+    int j=16,i=j+1;char option;
+    menu_start :
+    gotoxy(62,10);
+    printf("!!!Welcome to the Covid-19 Tracker!!!\n");
+    gotoxy(30,j-1);
+    printf("How do you wish to proceed?");
+    gotoxy(30,j+1);
+    printf("    Server Log-in");
+    gotoxy(30,j+2);
+    printf("    User Log-in");
+    gotoxy(30,i);
+    printf("-->");
+    option = _getch();
+    switch(option)
+    {
+        case 80 :   if(i<j+2)
+                        i++;
+                    clrscr();
+                    goto menu_start;
+                    break;
+        case 72 :   if(i>j+1)
+                        i--;
+                    clrscr();
+                    goto menu_start;
+                    break;
+        case 13 :   gotoxy(30,j+6);
+                    break;
+        default :   goto menu_start;
+    }
+    switch(i-j)
+    {
+        case 1  :   printf("CALL ADD FUNCTION");
+                    display_server_menu();
+                    break;
+        case 2  :   printf("CALL USER FUNCTION");
+                    display_user_menu();
+                    break;
+        default :   goto menu_start;
+    }
+
+}
+
